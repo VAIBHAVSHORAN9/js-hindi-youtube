e26e4f044f5e249201ad3d9034adf021d2dceff3
Vaibhav Shoran <89731210+VAIBHAVSHORAN9@users.noreply.github.com>
2024-05-25 09:25:26 +0000

    1.js updated

--- 01_basics/1.js
+++ 01_basics/1.js
@@ -1,1 +1,16 @@
-console.log("Vaibhav")
+const accountID = "12345"   // Only way to make cnstant in js
+let accountEmail = "abc@12345"
+var accountPassword = "hello"
+
+accountCity = "Jaipur"  // No keyword considered as var
+
+// accountID = "9"  // Not allowed
+accountEmail = "vaibhav"
+accountPassword = "what"
+accountCity = "Bengaluru"
+let name;   // undefined
+
+console.log(accountID)
+console.table([accountEmail, accountPassword, accountCity, name])
+
+// const hello; // Not allowed, const must be initialised
